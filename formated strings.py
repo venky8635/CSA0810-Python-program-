31eb51240c43b96c1786422be0f38f32138aab2a
venkateswarlu B <192465033.simats@saveetha.com>
2025-05-10 09:36:22 +0530

    Add files via upload

---
+++ formated strings.py
@@ -0,0 +1,3 @@
+name='tony'
+age=26
+print(f"{name} is {age} years old")
---
+++ print statements.py
@@ -0,0 +1,10 @@
+r=5
+pi=3.14159
+area=pi*r**2
+print("calculating the area of a circle")
+print("radius : ",r)
+print("pi: ",pi)
+print("using formula : area = pi * r *r")
+print("area = ",+float(pi)+"*"+float(r)+"*"+float(r))
+print(f"calculated area (f-string) : {area}")
+ 
